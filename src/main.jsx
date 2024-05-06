9de5149c9e76e54d5f9991088a1704e9e7dbc7d4
Mazhar1857 <ali.mazhar1857@gmail.com>
2024-05-06 15:36:28 +0530

    add HashRouter to enable routing in gh page

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/index.js'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 )
